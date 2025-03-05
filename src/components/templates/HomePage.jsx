682d9d5a87ce57fb0014ef8fa8feaa17d9b7cde9
Saman Ghapanuri <samanghapanuri@gmail.com>
2025-03-06 01:48:47 +0330

    Create services folder for managing API, httpRequests, data and etc...

--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -1,16 +1,21 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import TableCoins from "../modules/TableCoins";
+import { getCoinList } from "../../services/cryptoApi";
 
 const HomePage = () => {
   const [coins, setCoins] = useState([]);
+
   useEffect(() => {
-    fetch(
-      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&x_cg_demo_api_key=CG-usZXxCYCwoRSvQpq81ac3LsZ"
-    )
-      .then((res) => res.json())
-      .then((json) => setCoins(json));
+    const getData = async () => {
+      const res = await fetch(getCoinList());
+      const json = await res.json();
+      setCoins(json);
+    };
+
+    getData();
   }, []);
+
   return <TableCoins coins={coins} />;
 };
 
