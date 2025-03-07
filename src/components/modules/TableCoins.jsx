3b3845f14085ab813b68a5c7ddfd2c792ced2a48
Saman Ghapanuri <samanghapanuri@gmail.com>
2025-03-08 01:01:08 +0330

    Get data from Api and showing them

--- src/components/modules/TableCoins.jsx
+++ src/components/modules/TableCoins.jsx
@@ -1,6 +1,39 @@
 const TableCoins = ({ coins }) => {
   console.log(coins);
-  return <div>TableCoins</div>;
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Coin</th>
+            <th>Name</th>
+            <th>Price</th>
+            <th>24h</th>
+            <th>Total Volume</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {coins.map((coin) => (
+            <tr key={coin.id}>
+              <td>
+                <div>
+                  <img src={coin.image} alt="Image coin" />
+                  <span>{coin.symbol.toUpperCase()}</span>
+                </div>
+              </td>
+              <td>{coin.name}</td>
+              <td>${coin.current_price.toLocaleString()}</td>
+              {/* toLocaleString() method is for seperate numbers in 3 by comma(,) */}
+              <td>{coin.price_change_percentage_24h.toFixed(2)}%</td>
+              {/* toFixed(2) method means show till 2 number after point. */}
+              <td>{coin.total_volume.toLocaleString()}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default TableCoins;
